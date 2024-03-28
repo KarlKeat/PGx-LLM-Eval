a4f9ef46d1805001c2362a13a3b43495062f57a9
Karl  Keat <karlkeat@superman.pmacs.upenn.edu>
2024-03-28 17:00:00 -0400

    fix setup script

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -4,7 +4,8 @@ conda env create -f pgx_llm_maketests.yml
 
 wget https://files.cpicpgx.org/data/database/cpic_db_dump-v1.38.0.sql.gz
 
+conda activate pgx_llm_maketests
 initdb -D ./data
+pg_ctl -D ./data -l logfile start
 createdb cpic_db
-pg_ctl -D cpic_db -l logfile start
-gzip -c -d cpic_db_dump-v1.38.0.sql.gz > psql cpic_db
+gzip -c -d cpic_db_dump-v1.38.0.sql.gz | psql cpic_db
