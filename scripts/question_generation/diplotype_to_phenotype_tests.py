c366b4072f9a138d1f2d7a04f3cb5903d9ce1782
Karl <karlkeat@gmail.com>
2024-04-22 14:47:04 -0400

    add diplotype_to_phenotype tests

---
+++ scripts/question_generation/diplotype_to_phenotype_tests.py
@@ -0,0 +1,49 @@
+import psycopg2
+import json
+
+
+conn = None
+try:
+    conn = psycopg2.connect(
+        dbname='cpic_db',
+        host='localhost',
+    )
+except:
+    print("I am unable to connect to the database")
+
+
+# Get a cursor and execute select statement
+cursor = conn.cursor()
+
+query = """
+SELECT * from cpic.diplotype;
+"""
+
+cursor.execute(query)
+colnames = [desc[0] for desc in cursor.description]
+rows = cursor.fetchall()
+
+gene_dict = {}
+allele_dict = {}
+for row in rows:
+    gene = row[0]
+    diplotype = row[1]
+    phenotype = row[9]
+    if gene not in gene_dict:
+        gene_dict[gene] = set()
+    gene_dict[gene].add(phenotype)
+
+    allele_key = f"{gene}\t{diplotype}"
+    allele_dict[allele_key] = phenotype
+
+# Print out the results
+print("gene\tallele\tquestion\tanswer")
+for key in allele_dict:
+    gene_allele = key.replace('\t', ' ')
+    answer_choices = gene_dict[key.split('\t')[0]]
+    question = f"What is the pharmacogenetic phenotype for {gene_allele}? Please select the answer from the following choices: {answer_choices}, and respond with only your selection."
+    answer = allele_dict[key]
+    print(f"{key}\t{question}\t{answer}")
+
+# Close the connection when finished
+conn.close()
